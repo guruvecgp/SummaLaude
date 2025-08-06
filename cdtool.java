3be3f0a563956653b12e51a34b241e7629baf728
guruv@Guru-HP-6E.(none) <guru.vec.gp@gmail.com>
2025-08-06 14:45:15 +0530

    smams

--- cdtool.java
+++ cdtool.java
@@ -1,10 +1,16 @@
 
+
+
 public class okkala{
 public static void main(String[] args){
         AssHole a = new AssHole();
         int sum = 0;
         sum = a.b + a.c;
         System.out.println("summada " + sum);
+        Oli asss = new Oli();
+        int as = 0;
+        as = asss.c + asss.ams;
+        System.out.println("asdm " + as);
 
 }
 }
@@ -12,5 +18,10 @@ public static void main(String[] args){
 public class AssHole{
          int b=123;
          int c=123;
+} 
+
+public class Oli{
+     int c=124;
+     int ams=2345;
 }
 
