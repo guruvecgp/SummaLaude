4dbf7dae395a2e203ad16ac4032ffbedeb0cf596
guruv@Guru-HP-6E.(none) <guru.vec.gp@gmail.com>
2025-08-11 10:25:20 +0530

    sumnangati

--- cdtool.java
+++ cdtool.java
@@ -1,6 +1,4 @@
 
-
-
 public class okkala{
 public static void main(String[] args){
         AssHole a = new AssHole();
@@ -11,17 +9,28 @@ public static void main(String[] args){
         int as = 0;
         as = asss.c + asss.ams;
         System.out.println("asdm " + as);
-
+        Thakkali thak = new Thakkali(123);
+        int samba = 0;
+        samba = thak.aska + thak.kuska;
+        System.out.println("Sumnangatti " + samba);
 }
 }
 
+public class Thakkali{
+    int tuk=0;
+    public Thakkali(int tuk){
+         this.tuk = tuk;
+    }
+    int aska=tuk;
+    int kuska=343;
+}
 public class AssHole{
          int b=123;
          int c=123;
-} 
+}
 
 public class Oli{
-     int c=124;
+     int c=12434;
      int ams=2345;
 }
 
